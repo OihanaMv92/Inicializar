e3da6fe0fb422950e4c5d9c20663ee0439ddb914
Oihana Mendez <oihanamv92@gmail.com>
2021-10-21 00:49:45 +0200

    first commit

---
+++ src/es/Studium/Inicializar/Inicializar.java
@@ -0,0 +1,19 @@
+package es.Studium.Inicializar;
+ 
+public class Inicializar 
+{
+            public static void main(String[] args)
+            {
+                        final int TAM=2;
+                        int tabla[]=new int[TAM];
+                        
+                        for (int i=0;i<TAM;i++)
+                        {
+                                    tabla[i]=0;
+                        }
+                        for (int i=0;i<TAM;i++)
+                        {
+                                    System.out.print(tabla[i]+" ");
+                        }
+            }
+}
